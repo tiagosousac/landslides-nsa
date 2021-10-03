2b38d70d33803eb67783765d661f538232c69101
Tiago <tsc2@cin.ufpe.br>
2021-10-02 21:47:22 -0300

    adding skeleton of backend

---
+++ backend/app/exporter.py
@@ -0,0 +1,4 @@
+from app import app
+
+def exportToCsv(date):
+    return "Hello World"
--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,6 +1,9 @@
+from flask import render_template
 from app import app
+from app.exporter import *
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    return render_template('index.html')
